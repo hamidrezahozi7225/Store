cbbc1c27ae7ae0f7de5e4b9ac242f98e0a5d1b59
Hamid Hozi <hozihamidreza@gmail.com>
2022-07-31 16:40:03 +0430

    store project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import ProductContext from "./context/ProductContext";
 import Store from "./components/Store";
 import DetailProduct from "./components/DetailProduct";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import CartContext from "./context/CartContext";
 import NavBar from "./components/NavBar";
 import Buy from "./components/Buy";
@@ -18,6 +18,7 @@ function App() {
             <Route path="/product" element={<Store />} />
             <Route path="/product/:id" element={<DetailProduct />} />
             <Route path="/buy" element={<Buy />} />
+            <Route path="/" element={<Navigate to="/product"/>} />
           </Routes>
           <Footer />
         </CartContext>
